74105b4983475a6a9d7a0b571b21c308095ae35d
Paris DOUADY <paris.douady@hotmail.fr>
2021-08-19 11:33:15 +0200

    handle partial content correctly

--- musidex-daemon/src/domain/handlers.rs
+++ musidex-daemon/src/domain/handlers.rs
@@ -4,8 +4,9 @@ use crate::infrastructure::router::RequestExt;
 use crate::utils::get_file_range;
 use crate::Pg;
 use anyhow::{Context, Result};
-use hyper::{Body, Request, Response};
+use hyper::{Body, Request, Response, StatusCode};
 use serde_json::Value;
+use std::convert::TryInto;
 
 pub async fn metadata(req: Request<Body>) -> Result<Response<Body>> {
     let db = req.state::<Pg>();
@@ -31,7 +32,8 @@ pub async fn stream(req: Request<Body>) -> Result<Response<Body>> {
     let source_path = stream::stream_path(&c, id).await?;
     let file_path = format!("./storage/{}", source_path);
 
-    let r = if let Some(rangev) = req.headers().get(hyper::header::RANGE) {
+    let range_size;
+    let buf = if let Some(rangev) = req.headers().get(hyper::header::RANGE) {
         let range = http_range::HttpRange::parse_bytes(rangev.as_bytes(), u32::MAX as u64)
             .map_err(|_| anyhow!("could not decode range"))?;
         log::info!(
@@ -40,12 +42,22 @@ pub async fn stream(req: Request<Body>) -> Result<Response<Body>> {
             rangev.to_str().unwrap()
         );
         let r = range.get(0).context("no ranges")?;
-        Body::from(get_file_range(file_path, (r.start, r.start + r.length))?)
+        let buf = get_file_range(file_path, (r.start, r.start + r.length))?;
+        let len = r.start + buf.len() as u64;
+        range_size = (
+            r.start,
+            (r.start + r.length).min(len.saturating_sub(1)),
+            len,
+        );
+        buf
     } else {
-        Body::from(std::fs::read(file_path).context("failed reading source")?)
+        let buf = std::fs::read(file_path).context("failed reading source")?;
+        let l = buf.len() as u64;
+        range_size = (0, l.saturating_sub(1), l);
+        buf
     };
 
-    let mut r = Response::new(r);
+    let mut r = Response::new(Body::from(buf));
 
     let content_type = if source_path.ends_with("mp3") {
         "audio/mpeg"
@@ -60,6 +72,15 @@ pub async fn stream(req: Request<Body>) -> Result<Response<Body>> {
         .insert(hyper::header::CONTENT_TYPE, content_type.parse()?);
     r.headers_mut()
         .insert(hyper::header::ACCEPT_RANGES, "bytes".parse()?);
+    if req.headers().contains_key(hyper::header::RANGE) {
+        r.headers_mut().insert(
+            hyper::header::CONTENT_RANGE,
+            format!("bytes {}-{}/{}", range_size.0, range_size.1, range_size.2).try_into()?,
+        );
+        if range_size.0 > 0 && range_size.1 < range_size.2 {
+            *r.status_mut() = StatusCode::PARTIAL_CONTENT;
+        }
+    }
 
     Ok(r)
 }
