624d5c1321a699c55eab0c75b752865ac1bac4e2
Paris DOUADY <paris.douady@hotmail.fr>
2021-08-30 21:30:13 +0200

    undefined on array get

--- musidex-web/src/domain/entity.ts
+++ musidex-web/src/domain/entity.ts
@@ -67,7 +67,8 @@ export function dot(v1v: Vector, v2v: Vector): number {
     let v1 = v1v.v;
     let v2 = v2v.v;
     let d = 0;
-    for (let i=0; i < v1.length && i < v2.length; i++) {
+    for (let i = 0; i < v1.length && i < v2.length; i++) {
+        // @ts-ignore
         d += v1[i] * v2[i];
     }
     return d;
