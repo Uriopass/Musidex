19493a418eb7c70133e79287ab16355b7287a6ee
Paris DOUADY <paris.douady@hotmail.fr>
2021-09-26 17:56:56 +0200

    simplify small player

--- musidex-rn/components/SmallPlayer.tsx
+++ musidex-rn/components/SmallPlayer.tsx
@@ -1,45 +1,26 @@
 import React, {useContext} from "react";
 import {TracklistCtx} from "../common/tracklist";
-import {timeFormat, useUpdate} from "../common/utils";
-import {ControlsCtx, MetadataCtx, TrackplayerCtx} from "../constants/Contexts";
-import {StyleSheet, TouchableOpacity, View, ViewStyle} from "react-native";
+import {ControlsCtx, MetadataCtx} from "../constants/Contexts";
+import {Image, StyleSheet, TouchableOpacity, View, ViewStyle} from "react-native";
 import {TextFg} from "./StyledText";
 import {Icon} from "react-native-elements";
+import API from "../common/api";
 
 interface PlayerProps {
     style: ViewStyle,
 }
 
 const SmallPlayer = (props: PlayerProps) => {
-    const [trackplayer, dispatch] = useContext(TrackplayerCtx);
     const [metadata,] = useContext(MetadataCtx);
     const [doNext, doPrev] = useContext(ControlsCtx);
     const list = useContext(TracklistCtx);
 
-    let [, forceUpdate] = useUpdate();
-
-    /*
-    useEffect(() => {
-        trackplayer.audio.addEventListener("timeupdate", forceUpdate);
-        return () => trackplayer.audio.removeEventListener("timeupdate", forceUpdate);
-    }, [forceUpdate, trackplayer.audio])
-     */
-
-    const tags = metadata.getTags(trackplayer.current);
-    const curtime = 0;// todo
-    const duration = trackplayer.duration || (tags?.get("duration")?.integer || 0);
-    const trackProgress = duration > 0 ? curtime / duration : 0;
+    const tags = metadata.getTags(list.last_played[list.last_played.length - 1]);
     const title = (tags !== undefined) ? (tags.get("title")?.text || "No Title") : "No Title";
     const artist = tags?.get("artist")?.text || "No Artist";
     const thumbnail = tags?.get("compressed_thumbnail")?.text || (tags?.get("thumbnail")?.text || "");
 
-    let trackBarOnMove = (p: number) => {
-        dispatch({action: "setTime", time: p});
-    }
-
     const canPrev = list.last_played.length > 1;
-
-    console.log("lol", props.style);
 
     return (
         <View style={[styles.container, props.style]}>
@@ -47,7 +28,7 @@ const SmallPlayer = (props: PlayerProps) => {
                 {
                     (thumbnail !== "") &&
                     <View style={styles.currentTrackThumbnail}>
-                        {/*<Image src={"storage/" + thumbnail} alt="song cover"/>*/}
+                        <Image source={{uri: API.getAPIUrl() + "/storage/" + thumbnail}}/>
                     </View>
                 }
                 <View style={styles.currentTrackTitle}>
@@ -58,7 +39,7 @@ const SmallPlayer = (props: PlayerProps) => {
             <View style={styles.controls}>
                 <TouchableOpacity onPress={doPrev} disabled={!canPrev}>
                     <Icon size={50}
-                          color={canPrev?"white":"gray"}
+                          color={canPrev ? "white" : "gray"}
                           name="skip-previous"/>
                 </TouchableOpacity>
                 <TouchableOpacity onPress={() => doNext()}>
