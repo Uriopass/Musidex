2c34da3228b3b909ca526c925315dcce73d9798d
Paris DOUADY <paris.douady@hotmail.fr>
2021-10-03 23:30:53 +0200

    hide current playing sog from list

--- musidex-rn/components/Explorer.tsx
+++ musidex-rn/components/Explorer.tsx
@@ -7,12 +7,28 @@ import API from "../common/api";
 import Ctx from "../domain/ctx";
 import {NextTrackCallback} from "../common/tracklist";
 import {Icon} from "react-native-elements";
+import {SortBy, useMusicSelect} from "../common/filters";
 
 export default function Explorer() {
     const [metadata] = useContext(Ctx.Metadata);
+    const tracklist = useContext(Ctx.Tracklist);
+    const [doNext] = useContext(Ctx.Controls);
+    const [filters, setFilters] = useContext(Ctx.Filters);
+    const [user] = useContext(Ctx.User);
+    const [searchQry, setSearchQry] = useState("");
+    const [sortBy, setSortBy] = useState({kind: {kind: "similarity"}, descending: true} as SortBy);
+
+    const curTrack: number | undefined = tracklist.last_played[tracklist.last_played.length - 1];
+    const musics = useMusicSelect(metadata, searchQry, sortBy, tracklist, filters, user);
 
     return (
-        <SongList musics={metadata.musics}/>
+        <>
+            {curTrack &&
+            <SongElem musicID={curTrack} tags={metadata.getTags(curTrack) || new Map()} doNext={doNext} progress={1.0}
+                      progressColor="#1d2f23"/>
+            }
+            <SongList musics={musics}/>
+        </>
     )
 };
 const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
@@ -20,14 +36,24 @@ const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
 function SongList(props: { musics: number[] }) {
     const [metadata] = useContext(Ctx.Metadata);
     const [doNext] = useContext(Ctx.Controls);
+    const tracklist = useContext(Ctx.Tracklist);
 
     const flatRef = useRef<FlatList>(null);
     const topOpacity = useRef(new Animated.Value(0)).current;
     const [hidden, setHidden] = useState(true);
 
+    const curTrack: number | undefined = tracklist.last_played[tracklist.last_played.length - 1];
+
     const renderSong = useCallback(({item}: { item: number }) => {
-        return <SongElem musicID={item} tags={metadata.getTags(item) || new Map()} doNext={doNext}/>
-    }, [metadata]);
+        if (item === curTrack) {
+            return <></>;
+        }
+        return <SongElem musicID={item}
+                         tags={metadata.getTags(item) || new Map()}
+                         doNext={doNext}
+                         progress={tracklist.score_map.get(item)}
+                         progressColor="#28222f"/>
+    }, [metadata, doNext, tracklist, curTrack]);
 
     const onScroll = (ev: any) => {
         if (ev.nativeEvent.contentOffset.y <= 0.0) {
@@ -55,6 +81,9 @@ function SongList(props: { musics: number[] }) {
                   showsVerticalScrollIndicator={false}
                   renderItem={renderSong}
                   onScroll={onScroll}
+                  maxToRenderPerBatch={5}
+                  initialNumToRender={10}
+                  windowSize={5}
                   keyExtractor={(item) => item.toString()}
                   style={styles.musiclist}/>
         {!hidden &&
@@ -72,9 +101,11 @@ type SongElemProps = {
     musicID: number;
     tags: Map<string, Tag>;
     doNext: NextTrackCallback;
+    progress: number | undefined;
+    progressColor: string;
 }
 
-const SongElem = (props: SongElemProps) => {
+const SongElem = React.memo((props: SongElemProps) => {
     const cover = props.tags?.get("compressed_thumbnail")?.text || (props.tags?.get("thumbnail")?.text || "");
 
     const title = props.tags.get("title") || {music_id: props.musicID, key: "title", text: "No Title"};
@@ -82,6 +113,15 @@ const SongElem = (props: SongElemProps) => {
 
     return (
         <TouchableOpacity style={styles.item} onPress={() => props.doNext(props.musicID)}>
+            {
+                props.progress !== undefined &&
+                (
+                    <View style={[styles.progress, {
+                        width: (props.progress * 100) + "%",
+                        backgroundColor: props.progressColor
+                    }]}/>
+                )
+            }
             <Image style={styles.itemImage} source={{uri: API.getAPIUrl() + "/storage/" + cover}}/>
             <View style={styles.trackInfo}>
                 <TextFg>{title?.text}</TextFg>
@@ -89,9 +129,15 @@ const SongElem = (props: SongElemProps) => {
             </View>
         </TouchableOpacity>
     )
-}
+})
 
 const styles = StyleSheet.create({
+    progress: {
+        position: "absolute",
+        left: 0,
+        top: 0,
+        height: "100%",
+    },
     trackInfo: {
         padding: 10,
     },
@@ -108,6 +154,7 @@ const styles = StyleSheet.create({
         borderRadius: 50,
     },
     item: {
+        position: "relative",
         backgroundColor: Colors.fg,
         display: "flex",
         flexDirection: "row",
