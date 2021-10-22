35151dc2996626bc1d9f18ebdd2120cb59c7bb00
Paris DOUADY <paris.douady@hotmail.fr>
2021-10-22 13:43:01 +0200

    add build android script

---
+++ musidex-rn/build_android.sh
@@ -0,0 +1,4 @@
+npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
+rm -rf ./android/app/src/main/res/drawable-*
+rm -rf ./android/app/src/main/res/raw
+(cd android && ./gradlew assembleRelease)
