2c31bb60e9930bae3d2161809728a945cb54159b
Paris Douady <paris.douady@hotmail.fr>
2021-11-12 23:23:16 +0100

    bar light content

--- musidex-rn/App.tsx
+++ musidex-rn/App.tsx
@@ -1,61 +1,61 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+    /**
+     * Sample React Native App
+     * https://github.com/facebook/react-native
+     *
+     * Generated with the TypeScript template
+     * https://github.com/react-native-community/react-native-template-typescript
+     *
+     * @format
+     */
 
-import React, {useCallback} from 'react';
-import {Image, StatusBar, View} from 'react-native';
+    import React, {useCallback} from 'react';
+    import {Image, StatusBar, View} from 'react-native';
 
-import useCachedResources from "./domain/useCachedResources";
-import Navigation from "./navigation";
-import {SafeAreaProvider} from "react-native-safe-area-context";
-import Colors from "./domain/colors";
-import useStored from "./domain/useStored";
-import {emptyMetadata, MusidexMetadata, newMetadata} from "./common/entity";
-import API, {RawMusidexMetadata} from "./common/api";
-import Ctx from "./domain/ctx";
-import {LocalSettings, newLocalSettings} from "./domain/localsettings";
+    import useCachedResources from "./domain/useCachedResources";
+    import Navigation from "./navigation";
+    import {SafeAreaProvider} from "react-native-safe-area-context";
+    import Colors from "./domain/colors";
+    import useStored from "./domain/useStored";
+    import {emptyMetadata, MusidexMetadata, newMetadata} from "./common/entity";
+    import API, {RawMusidexMetadata} from "./common/api";
+    import Ctx from "./domain/ctx";
+    import {LocalSettings, newLocalSettings} from "./domain/localsettings";
 
-export default function App() {
-    const isLoadingComplete = useCachedResources();
-    const [metadata, setMetadata, loadedMeta] = useStored<MusidexMetadata>("metadata", emptyMetadata(), {
-        ser: (v: MusidexMetadata): string => {
-            return JSON.stringify(v.raw);
-        },
-        deser: (v: string): MusidexMetadata => {
-            const obj: RawMusidexMetadata = JSON.parse(v);
-            return newMetadata(obj);
-        },
-    });
+    export default function App() {
+        const isLoadingComplete = useCachedResources();
+        const [metadata, setMetadata, loadedMeta] = useStored<MusidexMetadata>("metadata", emptyMetadata(), {
+            ser: (v: MusidexMetadata): string => {
+                return JSON.stringify(v.raw);
+            },
+            deser: (v: string): MusidexMetadata => {
+                const obj: RawMusidexMetadata = JSON.parse(v);
+                return newMetadata(obj);
+            },
+        });
 
-    const [localSettings, setLocalSettings, loadedSettings] = useStored<LocalSettings>("local_settings", newLocalSettings());
+        const [localSettings, setLocalSettings, loadedSettings] = useStored<LocalSettings>("local_settings", newLocalSettings());
 
-    const [apiURL, setAPIUrl, loadedAPI] = useStored<string>("api_url", "");
-    API.setAPIUrl(apiURL);
+        const [apiURL, setAPIUrl, loadedAPI] = useStored<string>("api_url", "");
+        API.setAPIUrl(apiURL);
 
-    let fetchMetadata = useCallback(() => {
-        return API.getMetadata().then((meta) => {
-            if (meta === null) {
-                console.log("fetched null metadata?");
-                return;
-            }
-            setMetadata(meta);
-        })
-    }, [setMetadata]);
+        let fetchMetadata = useCallback(() => {
+            return API.getMetadata().then((meta) => {
+                if (meta === null) {
+                    console.log("fetched null metadata?");
+                    return;
+                }
+                setMetadata(meta);
+            })
+        }, [setMetadata]);
 
-    if (!isLoadingComplete || !loadedMeta || !loadedAPI || !loadedSettings) {
-        return <View style={{backgroundColor: '#383838', flex: 1, alignItems: "center", justifyContent: "center"}}>
-            <Image source={require('./musidex_logo.png')}/>
-        </View>;
-    } else {
-        return (
-            <SafeAreaProvider>
-                <StatusBar barStyle="dark-content" backgroundColor={Colors.bg}/>
+        if (!isLoadingComplete || !loadedMeta || !loadedAPI || !loadedSettings) {
+            return <View style={{backgroundColor: '#383838', flex: 1, alignItems: "center", justifyContent: "center"}}>
+                <Image source={require('./musidex_logo.png')}/>
+            </View>;
+        } else {
+            return (
+                <SafeAreaProvider>
+                    <StatusBar barStyle="light-content" backgroundColor={Colors.bg}/>
                 <Ctx.Metadata.Provider value={[metadata, fetchMetadata]}>
                     <Ctx.APIUrl.Provider value={[apiURL, setAPIUrl]}>
                         <Ctx.LocalSettings.Provider value={[localSettings, setLocalSettings]}>
