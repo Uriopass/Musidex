5ad82d648ef2cb0f1e6322e5b36fa337d38a9354
Paris DOUADY <paris.douady@hotmail.fr>
2021-11-15 13:39:09 +0100

    cap depending on OS

--- musidex-rn/domain/useCachedResources.ts
+++ musidex-rn/domain/useCachedResources.ts
@@ -7,6 +7,7 @@ import TrackPlayer, {
     CAPABILITY_SKIP_TO_NEXT,
     CAPABILITY_SKIP_TO_PREVIOUS
 } from 'react-native-track-player';
+import {Platform} from "react-native";
 
 export default function useCachedResources() {
     const [isLoadingComplete, setLoadingComplete] = React.useState(false);
@@ -17,16 +18,22 @@ export default function useCachedResources() {
             try {
                 await TrackPlayer.setupPlayer({iosCategory: 'playback', waitForBuffer: true, minBuffer: 5});
 
+                let cap = [
+                    CAPABILITY_PLAY,
+                    CAPABILITY_PAUSE,
+                    CAPABILITY_SEEK_TO,
+                    CAPABILITY_SKIP_TO_NEXT,
+                    CAPABILITY_SKIP_TO_PREVIOUS,
+                ];
+
+                if (Platform.OS === "android") {
+                    cap.push(CAPABILITY_JUMP_FORWARD);
+                }
+
                 await TrackPlayer.updateOptions({
                     stopWithApp: true,
                     // Media controls capabilities
-                    capabilities: [
-                        CAPABILITY_PLAY,
-                        CAPABILITY_PAUSE,
-                        CAPABILITY_SEEK_TO,
-                        CAPABILITY_SKIP_TO_NEXT,
-                        CAPABILITY_SKIP_TO_PREVIOUS,
-                    ],
+                    capabilities: cap,
 
                     // Capabilities that will show up when the notification is in the compact form on Android
                     compactCapabilities: [CAPABILITY_PLAY, CAPABILITY_PAUSE, CAPABILITY_SKIP_TO_NEXT, CAPABILITY_JUMP_FORWARD],
