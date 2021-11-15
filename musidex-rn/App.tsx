ac87bb1d5c0269ec9e18524d77161e5bea6aa3ff
Paris DOUADY <paris.douady@hotmail.fr>
2021-11-15 12:40:15 +0100

    reformat app.tsx

--- musidex-rn/App.tsx
+++ musidex-rn/App.tsx
@@ -1,64 +1,64 @@
-    /**
-     * Sample React Native App
-     * https://github.com/facebook/react-native
-     *
-     * Generated with the TypeScript template
-     * https://github.com/react-native-community/react-native-template-typescript
-     *
-     * @format
-     */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
 
-    import React, {useCallback} from 'react';
-    import {Image, StatusBar, View} from 'react-native';
+import React, {useCallback} from 'react';
+import {Image, StatusBar, View} from 'react-native';
 
-    import useCachedResources from "./domain/useCachedResources";
-    import Navigation from "./navigation";
-    import {SafeAreaProvider} from "react-native-safe-area-context";
-    import Colors from "./domain/colors";
-    import useStored from "./domain/useStored";
-    import {emptyMetadata, MusidexMetadata, newMetadata} from "./common/entity";
-    import API, {RawMusidexMetadata} from "./common/api";
-    import Ctx from "./domain/ctx";
-    import {LocalSettings, newLocalSettings} from "./domain/localsettings";
-    import {useMemoProv} from "./common/utils";
+import useCachedResources from "./domain/useCachedResources";
+import Navigation from "./navigation";
+import {SafeAreaProvider} from "react-native-safe-area-context";
+import Colors from "./domain/colors";
+import useStored from "./domain/useStored";
+import {emptyMetadata, MusidexMetadata, newMetadata} from "./common/entity";
+import API, {RawMusidexMetadata} from "./common/api";
+import Ctx from "./domain/ctx";
+import {LocalSettings, newLocalSettings} from "./domain/localsettings";
+import {useMemoProv} from "./common/utils";
 
-    export default function App() {
-        const isLoadingComplete = useCachedResources();
-        const [metadata, setMetadata, loadedMeta] = useStored<MusidexMetadata>("metadata", 0, emptyMetadata(), {
-            ser: (v: MusidexMetadata): string => {
-                return JSON.stringify(v.raw);
-            },
-            deser: (v: string): MusidexMetadata => {
-                const obj: RawMusidexMetadata = JSON.parse(v);
-                return newMetadata(obj);
-            },
-        });
+export default function App() {
+    const isLoadingComplete = useCachedResources();
+    const [metadata, setMetadata, loadedMeta] = useStored<MusidexMetadata>("metadata", 0, emptyMetadata(), {
+        ser: (v: MusidexMetadata): string => {
+            return JSON.stringify(v.raw);
+        },
+        deser: (v: string): MusidexMetadata => {
+            const obj: RawMusidexMetadata = JSON.parse(v);
+            return newMetadata(obj);
+        },
+    });
 
-        const [localSettings, setLocalSettings, loadedSettings] = useStored<LocalSettings>("local_settings", 1, newLocalSettings());
+    const [localSettings, setLocalSettings, loadedSettings] = useStored<LocalSettings>("local_settings", 1, newLocalSettings());
 
-        const [apiURL, setAPIUrl, loadedAPI] = useStored<string>("api_url", 0, "");
-        API.setAPIUrl(apiURL);
+    const [apiURL, setAPIUrl, loadedAPI] = useStored<string>("api_url", 0, "");
+    API.setAPIUrl(apiURL);
 
-        let fetchMetadata = useCallback(() => {
-            return API.getMetadata().then((meta) => {
-                if (meta === null) {
-                    console.log("fetched null metadata?");
-                    return;
-                }
-                setMetadata(meta);
-            })
-        }, [setMetadata]);
+    let fetchMetadata = useCallback(() => {
+        return API.getMetadata().then((meta) => {
+            if (meta === null) {
+                console.log("fetched null metadata?");
+                return;
+            }
+            setMetadata(meta);
+        })
+    }, [setMetadata]);
 
-        const metaa: [MusidexMetadata, any] = useMemoProv([metadata, fetchMetadata]);
+    const metaa: [MusidexMetadata, any] = useMemoProv([metadata, fetchMetadata]);
 
-        if (!isLoadingComplete || !loadedMeta || !loadedAPI || !loadedSettings) {
-            return <View style={{backgroundColor: '#383838', flex: 1, alignItems: "center", justifyContent: "center"}}>
-                <Image source={require('./musidex_logo.png')}/>
-            </View>;
-        } else {
-            return (
-                <SafeAreaProvider>
-                    <StatusBar barStyle="light-content" backgroundColor={Colors.bg}/>
+    if (!isLoadingComplete || !loadedMeta || !loadedAPI || !loadedSettings) {
+        return <View style={{backgroundColor: '#383838', flex: 1, alignItems: "center", justifyContent: "center"}}>
+            <Image source={require('./musidex_logo.png')}/>
+        </View>;
+    } else {
+        return (
+            <SafeAreaProvider>
+                <StatusBar barStyle="light-content" backgroundColor={Colors.bg}/>
                 <Ctx.Metadata.Provider value={metaa}>
                     <Ctx.APIUrl.Provider value={[apiURL, setAPIUrl]}>
                         <Ctx.LocalSettings.Provider value={[localSettings, setLocalSettings]}>
