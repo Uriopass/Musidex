2074aa0411dee69b1210e60f58198c88e0d5413e
Paris DOUADY <paris.douady@hotmail.fr>
2021-12-24 13:56:15 +0100

    try keeping screen lock

--- musidex-web/src/index.tsx
+++ musidex-web/src/index.tsx
@@ -11,6 +11,33 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+let wakeLock: any = null;
+
+// Function that attempts to request a wake lock.
+const requestWakeLock = async () => {
+    try {
+        if('wakeLock' in navigator) {
+            // @ts-ignore
+            wakeLock = await navigator.wakeLock.request('screen');
+            wakeLock.addEventListener('release', () => {
+                wakeLock = null;
+            });
+            console.log("screen lock acquired");
+        }
+    } catch (err) {}
+};
+
+const handleVisibilityChange = () => {
+    if (wakeLock !== null && document.visibilityState === 'visible') {
+        requestWakeLock();
+    }
+};
+
+requestWakeLock();
+
+document.addEventListener('visibilitychange', handleVisibilityChange);
+document.addEventListener('fullscreenchange', handleVisibilityChange);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
