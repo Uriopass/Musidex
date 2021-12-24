16068dc0775209670c9251a3285abad8edca0c39
Paris DOUADY <paris.douady@hotmail.fr>
2021-12-24 14:19:37 +0100

    fix nosleep import

--- musidex-web/src/index.tsx
+++ musidex-web/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import NoSleep from "nosleep.js";
+import NoSleep from "@uriopass/nosleep.js";
 
 ReactDOM.render(
     <React.StrictMode>
