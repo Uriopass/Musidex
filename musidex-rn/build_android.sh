98ae7293155e721cefc3bdce53d9bd155295f4a2
Paris DOUADY <paris.douady@hotmail.fr>
2025-05-13 13:29:50 +0200

    android release

--- musidex-rn/build_android.sh
+++ musidex-rn/build_android.sh
@@ -1,5 +1,7 @@
+echo "Don't forget to change versionCode and versionName in android/app/build.gradle"
 export NODE_OPTIONS=--openssl-legacy-provider
 npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res/
 rm -rf ./android/app/src/main/res/drawable-*
 rm -rf ./android/app/src/main/res/raw
 (cd android && ./gradlew bundleRelease)
+echo "built bundle in android/app/build/outputs/bundle/release"
